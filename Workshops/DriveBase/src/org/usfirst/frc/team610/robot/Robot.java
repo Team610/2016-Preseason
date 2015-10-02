1a4f2c9406e96597247b14798ef9c3be661f7c4a
Jamie <jamiekilburn@crescentschool.org>
2015-10-02 14:40:25 -0400

    Fix.

--- Workshops/DriveBase/src/org/usfirst/frc/team610/robot/Robot.java
+++ Workshops/DriveBase/src/org/usfirst/frc/team610/robot/Robot.java
@@ -2,8 +2,9 @@
 package org.usfirst.frc.team610.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -12,72 +13,64 @@ import edu.wpi.first.wpilibj.Talon;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-//Jack was here
 public class Robot extends IterativeRobot {
+
+	public static OI oi;
+
+    Command autonomousCommand;
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-	/*
-	 * 
-	 * TODO: CHECK TALON NUMBERS
-	 * 		 CHECK DIRECTION OF TALONS
-	 * 		 WHICH PRESEASON DRIVE 
-	 */
-	Talon leftFront,leftMid,leftBack,rightFront,rightMid,rightBack;
-	Joystick driver;
     public void robotInit() {
-    	leftFront = new Talon(0);
-    	leftMid = new Talon(1);
-    	leftBack = new Talon(2);
-    	rightFront = new Talon(3);
-    	rightMid = new Talon(4);
-    	rightBack = new Talon(5);
-    	driver = new Joystick(0);
-    	
-    	
-    	
+		oi = new OI();
+        // instantiate the command used for the autonomous period
+    }
+	
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
 
+    public void autonomousInit() {
+        // schedule the autonomous command (example)
+        if (autonomousCommand != null) autonomousCommand.start();
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    public void teleopInit() {
+		// This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to 
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        if (autonomousCommand != null) autonomousCommand.cancel();
+    }
+
+    /**
+     * This function is called when the disabled button is hit.
+     * You can use it to reset subsystems before shutting down.
+     */
+    public void disabledInit(){
 
     }
 
     /**
      * This function is called periodically during operator control
      */
-    //TOD
-    public void setLeft(double v){
-    	leftFront.set(v);
-    	leftMid.set(v);
-    	leftBack.set(v);
-    }
-    public void setRight(double v){
-    	rightFront.set(-v);
-    	rightMid.set(-v);
-    	rightBack.set(-v);
-    }
     public void teleopPeriodic() {
-    	double leftSpeed,rightSpeed,x,y;
-    	x = driver.getRawAxis(2);
-    	y = driver.getRawAxis(0);
-    	leftSpeed = y+x;
-    	rightSpeed = y-x;
-    	setLeft(leftSpeed);
-    	setRight(rightSpeed);
-    	
-        
+        Scheduler.getInstance().run();
     }
     
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-    
+        LiveWindow.run();
     }
-    
 }
---
+++ Workshops/DriveBase/src/org/usfirst/frc/team610/robot/constants/ElectricalConstants.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team610.robot.constants;
+
+public class ElectricalConstants {
+
+	/*To Add:
+	 * 6 Cim Drivetrain: Talons 0-5
+	 * 2 Mini-Cim Shooter: Talons 6-7
+	 * 
+	 * 
+	 * 
+	 */
+}
