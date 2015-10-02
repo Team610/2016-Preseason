0578bc80ba2e2527fb6872a7e086f0596fc51e22
jackw <jackw@10.10.210.41>
2015-10-02 15:54:08 -0400

    Started some of the Drivetrain

--- Workshops/DriveBase/src/org/usfirst/frc/team610/robot/subsystems/DriveTrain.java
+++ Workshops/DriveBase/src/org/usfirst/frc/team610/robot/subsystems/DriveTrain.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team610.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -11,8 +12,27 @@ public class DriveTrain extends Subsystem {
     // here. Call these from Commands.
 	
 	private static DriveTrain instance;
+	Talon leftFront, leftMid, leftBack, rightFront, rightMid, rightBack;
 	
 	DriveTrain(){
+		//CHECK PORTS
+		leftFront = new Talon(0);
+		leftMid = new Talon(1);
+		leftBack = new Talon(2);
+		rightFront = new Talon(3);
+		rightMid = new Talon(4);
+		rightBack = new Talon(5);
+	
+	}
+	
+	public static DriveTrain getInstance(){
+		if(instance == null){
+			instance = new DriveTrain();
+		}
+		return instance;
+	}
+	
+	public void setLeftSpeed(double v){
 		
 	}
 
