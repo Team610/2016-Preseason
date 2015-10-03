ed234bd0481fd392ddf05e33ea0da3cb76d11795
jackw <jackw@10.10.210.41>
2015-10-03 01:37:57 -0400

    Added functions for feeder and tray pistons

--- Workshops/DriveBase/src/org/usfirst/frc/team610/robot/subsystems/Shooter.java
+++ Workshops/DriveBase/src/org/usfirst/frc/team610/robot/subsystems/Shooter.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team610.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -15,6 +16,9 @@ public class Shooter extends Subsystem {
 	 DigitalInput optical; 
 	 boolean opt = false;
 	static Shooter inst;
+	
+	DoubleSolenoid feeder;
+	DoubleSolenoid tray;
 
 	 static Shooter getInstance(){
 		if(inst == null){
@@ -26,9 +30,29 @@ public class Shooter extends Subsystem {
 	 
 	  private Shooter(){
 		  shooter1 = new Victor(1);
-		  shooter2 = new Victor(2);		
-		  }
+		  shooter2 = new Victor(2);
+		  feeder = new DoubleSolenoid(0,1);
+		  tray = new DoubleSolenoid(2,3);
+	  }
+	  
+	  //Push feeder piston out
+	  public void feederOut(){
+		  feeder.set(DoubleSolenoid.Value.kForward);
+	  }
+	  public void feederIn(){
+		  feeder.set(DoubleSolenoid.Value.kReverse);
+	  }
 	 
+	  
+	  
+	  //WHICH ONE IS WHICH, CHECK PISTONS
+	  public void trayOpen(){
+		  
+	  }
+	  
+	  public void trayClose(){
+		  
+	  }
 
 	  public boolean optical(){
 		  return optical.get();
